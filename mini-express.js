396097932859bab1a38ce216e3e451e693d78c8d
luowendi <15622285015@163.com>
2019-05-12 12:10:18 +0800

    feat middleware

--- mini-express.js
+++ mini-express.js
@@ -11,7 +11,7 @@ class miniExpress {
         }
     }
 
-    register(path, callback) {
+    register(path) {
         const info = {};
         if (typeof path === 'string') {
             info.path = path;
@@ -20,21 +20,72 @@ class miniExpress {
             info.path = '/';
             info.stack = slice.call(arguments, 0)
         }
+        return info
     }
 
     use() {
-
+        const info = this.register.apply(this, arguments);
+        this.routes.all.push(info)
     }
 
     get() {
-
+        const info = this.register.apply(this, arguments);
+        this.routes.get.push(info)
     }
 
     post() {
-
+        const info = this.register.apply(this, arguments);
+        this.routes.post.push(info)
     }
 
-    listen() {
+    match(method, url) {
+        let stack = [];
+        if (url === '/favicon.ico') {
+            return stack
+        }
+
+        let curRoutes = []
+        curRoutes = curRoutes.concat(this.routes.all);
+        curRoutes = curRoutes.concat(this.routes[method]);
+        curRoutes.forEach((routeInfo) => {
+            if (url.indexOf(routeInfo.path) === 0) {
+                stack = stack.concat(routeInfo.stack)
+            }
+        });
+        return stack
+    }
+
+    callback() {
+        return (req, res) => {
+            res.json = (data) => {
+                // 设置返回格式
+                res.setHeader('Content-type', 'application/json');
+                res.end(JSON.stringify(data))
+            };
+            const url = req.url;
+            const method = req.method.toLowerCase();
+
+            const resultList = this.match(method, url)
+
+            this.handle(req, res, stack)
+        }
+    }
+
+    // 核心next机制
+    handle(req, res, stack) {
+      const next = () => {
+          const middleware = stack.shift();
+          if(middleware){
+              middleware(req,res,next)
+          }
+      };
+
+      next()
+    }
+
+    listen(...args) {
+        const server = http.createServer(this.callback());
+        server.listen(...args)
 
     }
 }
